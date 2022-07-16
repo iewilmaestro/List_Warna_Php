6bf533832faf099d70a13ff00b8617553cf91c89
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2022-07-16 23:02:04 +0700

    Update run.php

--- run.php
+++ run.php
@@ -2,34 +2,34 @@
 
 function List_Warna_String(){
 	$Warna_String = [
-		'black' 		=> '0;30',
+		'black' 	=> '0;30',
 		'dark_gray' 	=> '1;30',
-		'blue' 			=> '0;34',
+		'blue' 		=> '0;34',
 		'light_blue' 	=> '1;34',
-		'green' 		=> '0;32',
+		'green' 	=> '0;32',
 		'light_green' 	=> '1;32',
-		'cyan' 			=> '0;36',
+		'cyan' 		=> '0;36',
 		'light_cyan' 	=> '1;36',
-		'red' 			=> '0;31',
+		'red' 		=> '0;31',
 		'light_red' 	=> '1;31',
-		'purple' 		=> '0;35',
+		'purple' 	=> '0;35',
 		'light_purple' 	=> '1;35',
-		'brown' 		=> '0;33',
-		'yellow' 		=> '1;33',
+		'brown' 	=> '0;33',
+		'yellow' 	=> '1;33',
 		'light_gray' 	=> '0;37',
-		'white'		 	=> '1;37'
+		'white'		 => '1;37'
 	];
 	return $Warna_String;
 }
 function List_Warna_Latar(){
 	$Warna_Latar = [
-		'black' 		=> '40',
-		'red' 			=> '41',
-		'green' 		=> '42',
-		'yellow'	 	=> '43',
-		'blue' 			=> '44',
-		'magenta' 		=> '45',
-		'cyan' 			=> '46',
+		'black' 	=> '40',
+		'red' 		=> '41',
+		'green' 	=> '42',
+		'yellow'	 => '43',
+		'blue' 		=> '44',
+		'magenta' 	=> '45',
+		'cyan' 		=> '46',
 		'light_gray' 	=> '47'
 	];
 	return $Warna_Latar;
