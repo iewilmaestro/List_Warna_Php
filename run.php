95a01d45b4da741958932edb9caadfeeb0022602
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2022-07-16 23:04:06 +0700

    Update run.php

--- run.php
+++ run.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+Judul   : Simpel fungsi warna
+Editor  : iewil
+Github  : https://github.com/iewilmaestro
+Sumber  : https://www.if-not-true-then-false.com/2010/php-class-for-coloring-php-command-line-cli-scripts-output-php-output-colorizing-using-bash-shell-colors/
+*/
+
 function List_Warna_String(){
 	$Warna_String = [
 		'black' 	=> '0;30',
